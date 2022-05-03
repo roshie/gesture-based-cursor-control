611ccdbd68fa9978086d62ce54a2861ecfe3b7b1
Roshita <roshitha.23it@licet.ac.in>
2022-05-03 22:10:59 +0530

    init mouse control class

---
+++ MouseControls.py
@@ -0,0 +1,34 @@
+import pyautogui as pyag
+from playsound import playsound
+
+# Import Click sound
+clickSound = 'sounds/MouseClick.mp3'
+
+class MouseControls():
+    def __init__(self, drag, scrollOffset):
+        self.drag = drag # values - 40 to 50
+        self.scrollOffset = scrollOffset
+        
+    def moveMouse(self, direction: str):
+        if direction == 'right':
+            pyag.moveRel(self.drag, 0)
+        elif direction == 'left':
+            pyag.moveRel(-self.drag, 0)
+        elif direction == 'up':
+            pyag.moveRel(0, -self.drag)
+        elif direction == 'down':
+            pyag.moveRel(0, self.drag)
+                
+    def scrollVertically(self, direction: str):
+        if direction == 'right':
+            pyag.moveRel(self.drag, 0)
+        elif direction == 'left':
+            pyag.moveRel(-self.drag, 0)
+        elif direction == 'up':
+            pyag.scroll(self.scrollOffset)
+        elif direction == 'down':
+            pyag.scroll(-self.scrollOffset)
+
+    def click(self, button: str):
+        pyag.click(button=button)
+        # playsound(clickSound)
